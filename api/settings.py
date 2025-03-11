19d7433815154d013b31650e2e937b069259fcb1
nrjss67 <extazzzyprofit@gmail.com>
2025-03-11 20:53:56 +0200

    fixed comments

--- api/settings.py
+++ api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ke*j#+^o8ee37^+%e*vnwyqgth74bkgojx$3r50ixo!bbz-s)e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
--- cinema/views.py
+++ cinema/views.py
@@ -19,7 +19,7 @@ def movie_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     
 @api_view(["GET", "PUT", "DELETE"])
@@ -34,7 +34,7 @@ def movie_detail(request, pk):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
